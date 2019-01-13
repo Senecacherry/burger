b3923f33c6d4ce3730d9f94f686ed454263a6baa
Senecacherry <42197371+Senecacherry@users.noreply.github.com>
2019-01-12 17:45:16 -0700

    Files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Double Bacon');
+INSERT INTO burgers (name) VALUES ('Black & Blue');
+INSERT INTO burgers (name, eaten) VALUES ('Pretzel Burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('Double Cheeseburger', true);
+INSERT INTO burgers (name, eaten) VALUES ('Peanut Butter Burger', true);
+INSERT INTO burgers (name) VALUES ('Obama Burger');
